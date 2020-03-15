eba08999004e1f8146b567e4822f6337f5af0fa1
Anmol Karn <anmol.karan123@gmail.com>
2020-03-15 18:21:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int main(void)
     printf("Press 1 to calculate area of circle\n"); 
     printf("Press 2 to calculate area of square\n"); 
     printf("Press 3 to calculate area of sphere\n"); 
+    printf("Press 4");
     printf("Enter your choice:\n"); 
     choice = input(); 
     
@@ -75,78 +76,3 @@ int main(void)
     
     
     
-    
-    
-        
-//             int choice;
-//         
-//         do
-//         {
-//         printf("********** Main Menu **********\n(1).Calculator\n(2).Decimal to Binary convertor\n(3).Perfect number finder\n(4).Prime number or composite number\n(5).Factorial Calculator\n(6).To exit\n");
-//         scanf("%d",choice);
-// 
-//         switch (choice)
-//         {
-//         case 1:
-//                         system("cls");
-//                         printf("********** Calculator **********\n\nEnter numbers:-\n");
-//                         
-//                         libr();
-//                         printf("(0). Return to main menu");
-//                         printf("\n");
-//                     break;
-// 
-//                 case 2:
-//                     system("cls");
-// 
-//                     printf("********** Decimal to Binary convertor **********\n\nEnter an integer in decimal number system:-");
-//                 scanf("%d", &bininput);
-// 
-//                 printf("\n(0). Return to main menu\n");
-//                 printf("\n");
-//                 printf("\n");
-//                 break;
-// 
-//             case 3:
-//                 system("cls");
-// 
-//                 printf("********** Perfect number finder **********\n");
-//                 printf("Enter a number:- ");
-//             scanf("%d" ,&perfectinput);
-//                 
-//                 printf("(0). Return to main menu\n");
-//                 break;
-// 
-//             case 4:
-//                 system("cls");
-// 
-// 
-//                 printf("********** Prime number or composite number **********\n");
-//                 printf("Enter a number:- ");
-//                 scanf("%d" ,&primeinput);
-//                 
-//                 printf("(0). Return to main menu\n");
-//                 break;
-// 
-//             case 5:
-//                 system("cls");
-// 
-//                 printf("********** Factorial Calculator **********\n");
-//                 printf("Enter a number:- ");
-//                 scanf("%d" ,&factinput);
-//                 
-//                 printf("(0). Return to main menu\n");
-//                 break;
-// 
-//             case '6':
-//                     system("cls");
-//                     printf("Program terminated.\nPress any key to exit...");
-//                     break;
-//             default: printf("wrong input!...select a valid option!");
-//             }
-//         }while(choice != 6);
-//         printf("bye");
-//         
-//         
-        
-}
